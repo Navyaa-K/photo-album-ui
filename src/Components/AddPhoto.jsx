1c5645750ed98b615e80a96304c1ee8613cec47e
Navyaa-K <navyakannan04@gmail.com>
2025-07-15 01:09:49 +0530

    uodated

--- src/Components/AddPhoto.jsx
+++ src/Components/AddPhoto.jsx
@@ -16,7 +16,7 @@ export const AddPhoto = () => {
         <div className="container-fluid">
             <div className="row">
                 <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                    <div className="row g-3 px-3">
+                    <div className="row g-3 ">
                         <div className="col col-6 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                             <label htmlFor="" className="form-label">AlbumId</label>
                             <input type="text" className="form-control" />
--- src/Nav.jsx
+++ src/Nav.jsx
@@ -5,11 +5,6 @@ export const Nav = () => {
   return (
     <div>
 
-        <div className="container-fluid">
-            <div className="row">
-                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                    <div className="row g-3 px-4">
-                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                                 <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
     <a class="navbar-brand" href="#">PhotoAlbum</a>
@@ -34,11 +29,7 @@ export const Nav = () => {
     </div>
   </div>
 </nav>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+                       
     </div>
   )
 }
